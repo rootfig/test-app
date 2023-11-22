30692daccc2b86b9f4b6efa40a2265784d3f2912
Vadim <rootfig@gmail.com>
2023-11-22 10:15:14 +0300

    Отпраит форму в компонент

---
+++ src/components/form-post/form-post.tsx
@@ -0,0 +1,46 @@
+import { FormEvent, useState } from "react";
+import { useAddPostMutation } from "../../api/postsApi";
+
+function FormPost() {
+  const [newName, setNewName] = useState("");
+  const [newPost, setNewPost] = useState("");
+  const [addPost] = useAddPostMutation();
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    await addPost({ name: newName, post: newPost }).unwrap();
+    setNewName("");
+    setNewPost("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className='contact'>
+        <div className="contact-input">
+          <label htmlFor="name">Ваше имя: </label>
+          <input
+            id="name"
+            type="text"
+            required
+            autoFocus
+            placeholder="Tonny"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+        </div>
+        <div className="contact-input">
+          <label htmlFor="post">Ваша мысль: </label>
+          <textarea
+            id="post"
+            required
+            placeholder='Люблю котят'
+            value={newPost}
+            onChange={(e) => setNewPost(e.target.value)}
+          />
+        </div>
+        <button type='submit'>Добавить пост</button>
+      </form>
+  )
+}
+
+export default FormPost
--- src/components/posts-list/posts-list.tsx
+++ src/components/posts-list/posts-list.tsx
@@ -1,26 +1,19 @@
 
-import { useGetPostsQuery, useAddPostMutation, useDeletePostMutation } from '../../api/postsApi';
+import { useGetPostsQuery, useDeletePostMutation } from '../../api/postsApi';
 import { Post } from '../../types/postsType';
-import { FormEvent, useState } from 'react';
+import { useState } from 'react';
 import PostItem from '../post-item/post-item';
 import { LIMIT_SHOW_STEP } from '../../common';
+import FormPost from '../form-post/form-post';
 
 function PostsList() {
-  const [newName, setNewName] = useState("");
-  const [newPost, setNewPost] = useState("");
+  
   const [limit, setNewLimit] = useState(5)
   const { data, isLoading } = useGetPostsQuery(limit);
-  const [addPost] = useAddPostMutation();
+  
   const [deletePost] = useDeletePostMutation();
 
-  const handleSubmit = async (event: FormEvent) => {
-    event.preventDefault();
-
-    await addPost({ name: newName, post: newPost }).unwrap();
-    setNewName("");
-    setNewPost("");
-  }
-
+  
   const handleDeletePost = async (id: number) => {
     await deletePost(id).unwrap();
   }
@@ -29,31 +22,7 @@ function PostsList() {
 
   return (
     <div className='container'>
-      <form onSubmit={handleSubmit} className='contact'>
-        <div className="contact-input">
-          <label htmlFor="name">Ваше имя: </label>
-          <input
-            id="name"
-            type="text"
-            required
-            autoFocus
-            placeholder="Tonny"
-            value={newName}
-            onChange={(e) => setNewName(e.target.value)}
-          />
-        </div>
-        <div className="contact-input">
-          <label htmlFor="post">Ваша мысль: </label>
-          <textarea
-            id="post"
-            required
-            placeholder='Люблю котят'
-            value={newPost}
-            onChange={(e) => setNewPost(e.target.value)}
-          />
-        </div>
-        <button type='submit'>Добавить пост</button>
-      </form>
+      <FormPost />
       <ul>
         {data.map((post: Post) => (
           <li key={post.id} >
